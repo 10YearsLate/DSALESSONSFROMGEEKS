9086c01ad580ba49317ca9fd484268f0087ef83c
10YearsLate <souvik.itconsultant@gmail.com>
2019-10-29 23:36:03 +0530

    Primality check

---
+++ Mathematics/primality.java
@@ -0,0 +1,23 @@
+/*Prime number can always be represented as 6n+1 or 6n-1 except 2 & 3
+
+public class primality{
+
+	public static void main(String[] args){
+
+		for (int i=0;i<args.length;i++){
+			
+			System.out.println(args[i]);
+		}
+
+		int number=57;
+
+		if((number-1)%6==0 || (number+1)%6==0 || number==2 ||number==3) 
+			System.out.println("Number is Prime");
+		else	System.out.println("Number is not Prime");
+			
+		
+
+	}
+
+
+}
