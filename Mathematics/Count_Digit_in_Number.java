d4ec03c3f8e187313bdc8d3b7cb41b1dd59386ba
10YearsLate <souvik.itconsultant@gmail.com>
2019-10-29 21:25:41 +0530

    Added few mathematical problem

--- Mathematics/Count_Digit_in_Number.java
+++ Mathematics/Count_Digit_in_Number.java
@@ -12,6 +12,28 @@ public class Count_Digit_in_Number{
 	}
 
 
+	public static float Celcius_Farenhite(int celcius){
+
+		return ((celcius/5)*9)+32;
+
+	}
+
+	public static void find_quadratic_root(int a,int b,int c){
+
+		double discriminant= Math.sqrt(Math.pow(b,2)-(4*a*c));
+
+		double first_root=(b*(-1) + discriminant)/(2*a);
+
+		double second_root=(b*(-1) - discriminant)/(2*a);
+
+		double[] root= {first_root ,second_root};
+		for(int i=0;i<root.length;i++){
+			System.out.println(root[i]);
+		}
+
+	}
+
+
 	public static void main(String[] args){
 
 
@@ -30,6 +52,17 @@ public class Count_Digit_in_Number{
 
 		
 		System.out.println("Count from logarithmic solution  "+(1+Math.floor(Math.log10(349))));
+
+
+		System.out.println("This is how you print absolute value "+Math.abs(-3));
+
+		
+		System.out.println("This happens when you convert 50C 2 F  "+ Celcius_Farenhite(50));
+
+		find_quadratic_root(1,2,1);
+
+
+
 		
 
 		
