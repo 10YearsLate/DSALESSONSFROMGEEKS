eba101eaa42e2a4e5fc940223044b29499083267
10YearsLate <souvik.itconsultant@gmail.com>
2019-10-30 05:06:09 +0000

    fixed primality bug

--- Mathematics/primality.java
+++ Mathematics/primality.java
@@ -1,5 +1,5 @@
 /*Prime number can always be represented as 6n+1 or 6n-1 except 2 & 3*/
-
+import java.lang.Math;
 public class primality{
 
 	public static void main(String[] args){
@@ -9,13 +9,24 @@ public class primality{
 			System.out.println(args[i]);
 		}
 
-		int number=57;
+		int number=5437;
+        boolean prime=true;
+		//if((number-1)%6==0 || (number+1)%6==0 || number==2 ||number==3) Bug found not working for 35
+        for(int i=2;i<=Math.sqrt(number);i++){
 
-		if((number-1)%6==0 || (number+1)%6==0 || number==2 ||number==3)
-			System.out.println("Number is Prime");
-		else	System.out.println("Number is not Prime");
+            //System.out.println(Math.sqrt(number));
 
+            if(Math.floor(Math.sqrt(number))%i==0){
 
+                if(number%i==0){
+
+                    System.out.println(number+" Number is not Prime "+ i);
+                    prime=false;
+                    break;
+                }
+            }
+        }
+			if(prime) System.out.println("Number is Prime");
 
 	}
 
