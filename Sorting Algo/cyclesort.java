8fc9608fd92bfe3a780316161dcd2d4806f155c4
10YearsLate <souvik.itconsultant@gmail.com>
2019-11-02 19:06:13 +0000

    bug fixed

--- Sorting Algo/cyclesort.java
+++ Sorting Algo/cyclesort.java
@@ -1,4 +1,4 @@
-/*abstract
+ /*abstract
 
 Algo:
 
@@ -18,58 +18,59 @@ public class cyclesort{
 
     public static void cycle(int[] a,int n){
 
-        //int start=0;
 
-        //int pos=0;
         for(int start=0;start<=n-2;start++){
             int item=a[start];
-
             int pos=start;      //another bug found
-            for(int i=start+1;i<n;i++)
+            for(int i=start+1;i<n;i++){
                 if(a[i]<item)
                 pos++;
+            }
 
             if(pos==start) continue;
 
-            while(item==a[pos]) pos++;
+            while(item==a[pos]) pos+=1;
 
             if(pos!=start){
-                int temp=a[pos];
-                a[pos]=item;
-                item=temp;
+                int temp=item;
+                item=a[pos];
+                a[pos]=temp;
             }
 
             while(pos!=start){
+                //System.out.print("while apos" +a[pos]+""+item+" "+pos);
                 pos=start;
 
                 for(int i=start+1;i<n;i++){
 
-                    if(a[i]<item){
-                        pos++;
+                    if(a[i]<item)
+                        pos+=1;
                     }
                     //if(pos==start) continue;
 
-                    while(item==a[pos]) pos++;
+                    while(item==a[pos]) pos+=1;
 
-                    if(a[pos]!=item){
-                        int temp=a[pos];
-                        a[pos]=item;
-                        item=temp;
+                    if(item != a[pos]){
+                        int temp=item;
+                        item=a[pos];
+                        a[pos]=temp;
 
                     }
+
+
                 }
 
             }
 
         }
-    }
+
 
 
     public static void main(String[] args){
-        int[] a={2,8,1,6,2,6};
+        int[] a={ 1, 8, 3, 9, 10, 10, 2, 4 };
         cycle(a,a.length);
-        for(int i=0;i<a.length;i++){
-            System.out.print(a[i] + " ");
+        for(int i1=0;i1<a.length;i1++){
+            System.out.print(a[i1] + " ");
         }
     }
 }
