4af3d8ac64288f143fe364fa684aa923a7c24677
10YearsLate <souvik.itconsultant@gmail.com>
2019-11-03 08:05:47 +0000

    Binary Search Added with recursion

--- Searching/BinarySearch.java
+++ Searching/BinarySearch.java
@@ -10,30 +10,33 @@ else if(arr[mid]<item arr,low,mid-1,item) else bsearc(a,mid+1,high,item)
 
 */
 
-public class bsearch{
+public class BinarySearch{
 
   public static int bsearch(int[] a,int l,int h,int item){
-  
+
     int mid=l+(h-l)/2;
-    if(mid==low && a[mid]!=item) return -1;
-    if(mid==high && a[mid]!=item] return -1;
-    if(a[mid]==item) return mid;
+    int pos=-1;
+    //System.out.println(a[mid]+" "+item);
+    if(mid==l && a[mid]!=item) return pos;
+    if(mid==h && a[mid]!=item) return pos;
+    if(a[mid]==item) pos=mid;
     else{
-      if(a[mid]>item) bsearch(a,l,mid-1;item);
-      else bsearch(a,mid+1,h,item);
-    
+      if(a[mid]>item) pos=bsearch(a,l,mid-1,item);
+      else pos=bsearch(a,mid+1,h,item);
+
     }
-  
+    return pos;
+
   }
 
 
   public static void main(String[] args){
-  
+
     int[] a={1,2,3,4,5,6,7};
     int item=5;
     int index=bsearch(a,0,6,5);
     System.out.println(index);
-  
+
   }
 
 
