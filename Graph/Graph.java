306d5aa6edbfd8e4f2645241662e9ca4685a6c30
10YearsLate <souvik.itconsultant@gmail.com>
2019-11-06 17:38:11 +0000

    Implementing Graph

--- Graph/Graph.java
+++ Graph/Graph.java
@@ -1,28 +1,30 @@
+import java.util.*;
+
 public class Graph{
 
-  public static void addedge(ArrayList arr,int s,int d){
-  
+  public static void addedge(ArrayList<ArrayList<Integer>> arr,int s,int d){
+
     arr.get(s).add(d);
     arr.get(d).add(s);
-  
+
   }
-  
-  public static bfs(ArrayList<Integer> arr,int source){
-  
+
+  public static void bfs(ArrayList<Integer> arr,int source){
+
   }
 
 
   public static void main(String[] args){
-  
+
     int v=5;
     ArrayList<ArrayList<Integer>> arr=new ArrayList<ArrayList<Integer>>(5);
-    
-    for(int i=0;i<v;arr++){
-        arr[i]=new ArrayList<Integer>();
-        
+
+    for(int i=0;i<v;i++){
+        arr.add(new ArrayList<Integer>());
+
     }
-    
-  
+
+
   }
 
 }
