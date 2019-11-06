be72892689edff32a5bb0248df5ef454679120d6
10YearsLate <souvik.itconsultant@gmail.com>
2019-11-06 07:36:28 +0000

    Array Reversal

--- Array/Reverse.java
+++ Array/Reverse.java
@@ -4,29 +4,29 @@
 public class Reverse{
 
     public static void main(String[] args){
-    
-    int a= {1,2,3,4,5,6,7};
-    
+
+    int[] a= {1,2,3,4,5,6,7};
+
     int low=0,high=a.length-1;
-    
+
     while(low < high){
-    
+
         int temp=a[low];
         a[low]=a[high];
-        a[high]=a[low];
+        a[high]=temp;
         low++;
         high--;
-    
+
     }
-    
+
     for(int i=0;i<a.length;i++){
-    
-      System.out.println(a[i]);
+
+      System.out.print(a[i]+" ");
     }
-    
-      
-    
-    
+
+
+
+
     }
 
 }
