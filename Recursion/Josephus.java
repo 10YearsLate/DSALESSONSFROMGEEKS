0768c2ca570836dc739569276ba9ca517127aa25
10YearsLate <souvik.itconsultant@gmail.com>
2019-11-09 12:34:42 +0000

    learned josephus

--- Recursion/Josephus.java
+++ Recursion/Josephus.java
@@ -1,17 +1,23 @@
+/*
+
+41=2^5+9  answer . 2^9+1=19
+*/
+
 public class Josephus{
 
   public static int joseutil(int n,int k){
-  
+
       if(n==1) return n;
       else {
-        int op=((joseutil(n-1,k))%n)+1;
+        int op=((joseutil(n-1,k)+k-1)%n)+1;
+        return op;
       }
-  
+
   }
 
   public static void main(String[] args){
-  
-      System.out.println(joseutil(3,2));
+
+      System.out.println(joseutil(41,2));
   }
 
 }
