77a38fcb250d226493e25694ee57c0c171e1c26d
10YearsLate <souvik.itconsultant@gmail.com>
2019-11-09 09:42:23 +0000

    Stringpermu with recusrion

--- Recursion/StringPermutation.java
+++ Recursion/StringPermutation.java
@@ -1,34 +1,37 @@
 public class StringPermutation{
 
   private void permute(String str,int l,int h){
-      
+
       if(l==h) System.out.println(str+" ");
       else{
         for(int i=l;i<=h;i++){
-          swap(str,i,l);
+          str=swap(str,i,l);
           permute(str,l+1,h);
-          swap(str,i,l);
-        
+          str=swap(str,i,l);
+
         }
       }
-  
+
   }
-  
-  private void swap(String str,int l,int h){
-  
-    Char[] ch=str.toCharArray();
+
+  private String swap(String str,int l,int h){
+
+    char[] ch=str.toCharArray();
     char temp=ch[l];
     ch[l]=ch[h];
     ch[h]=temp;
+    //str=ch.toString();
+    return String.valueOf(ch);
+
   }
 
 
   public static void main(String[] args){
-  
+
       String str="ABC";
       StringPermutation sp=new StringPermutation();
-      sp.permute(str,0,(str.length-1));
-      
+      sp.permute(str,0,(str.length()-1));
+
   }
 
 }
