4ca319ff0e3933d54a73d53688456cd78352a8df
10YearsLate <souvik.itconsultant@gmail.com>
2019-11-17 17:32:49 +0000

    Activity selection problem

--- Greedy/ActivitySelection.java
+++ Greedy/ActivitySelection.java
@@ -5,20 +5,30 @@ public class ActivitySelection{
   public static void main(String[] args){
   
     int no_of_acty=3;
+    int count=0;
     pair[] activities=new pair[no_of_acty];
     
     activities[0]=new pair(12,25);
     activities[1]=new pair(10,20);
     activities[2]=new pair(20,30);
     
-    Arrays.sort(activities,pair);
+    Arrays.sort(activities,new pair());
     
-    System.out.println(activities);
-  
+    pair prev=new pair(-1,-1);
+    for(pair a: activities){
+            if(prev.end<=a.start){
+                System.out.println(a.start+" "+a.end);
+                prev=a;
+                count++;
+
+            }
+
+    }
+    System.out.print("Total number of Activities can be scheduled "+count);
   }
 }
 
-class pair implements comparator<pair>{
+class pair implements Comparator<pair>{
   int start;
   int end;
   
@@ -26,7 +36,11 @@ class pair implements comparator<pair>{
     this.start=start;
     this.end=end;
   }
-  @override
+  
+    public pair() {
+    }
+
+    @Override
   public int compare(pair a,pair b){
     if (a.end==b.end) return 0;
     if(a.end>b.end) return 1;
