81b8bc6d98ed1a62efb14e34c1d0c6d94a5de3ed
10YearsLate <souvik.itconsultant@gmail.com>
2019-11-21 09:31:00 +0530

    Corrected BIT with update, sum and sum range query

--- Tree/BIT.java
+++ Tree/BIT.java
@@ -1,55 +1,46 @@
 public class BIT{
-
-
-    public static void main(String[] args){
-
-        int[] a={1,1,2,2,3,4,4,5,5};
-
-        BIT bit=new BIT();
-
-        int[] b=new int[a.length+1];
-
-        for(int i=0;i<a.length;i++){
-            bit.update(1,a[i],b);
-        }
-
-        for(int i=1;i<b.length;i++){
-            System.out.print(b[i]+" ");
-            
-            //System.out.print(bit.sum(1, b));
-        }
-        System.out.println("");
-        for(int i=1;i<b.length;i++){
-            //System.out.print(b[i]+" ");
-            //System.out.println("");
-            System.out.print(bit.sum(i, b)+" ");
-        }
-
-        
-
-    }
-
-    private int sum(int idx,int[] bit) {
-            int s = 0;
-            while(idx > 0) {
-                s += bit[idx];
-                idx -= idx & (-idx);
-            }
-        return s;
-    }
-
-    
-
-
-    private void update(int val, int index, int[] b) {
-
-        while(index<b.length){
-            //System.out.print(index+" ");
-            b[index]+=val;
-            
-            index+=(index & (-index));
-        }
-    }
-
-    
+	
+	static int[] a={1,2,3,4,5,2,2,3};
+	static int[] bit=new int[a.length+1];
+	
+	public static void update(int index,int val){
+	
+		while(index<=a.length){
+			
+			bit[index]+=val;
+			index += index & (-index);
+			
+		}
+	
+	
+	}
+	
+	public static int sum (int index){
+		int sum=0;
+		
+		while(index>0){
+			
+			sum +=bit[index];
+			index -= index &(-index);
+			
+		}		
+		return sum;
+		
+	}
+	
+	public static void main(String[] args){
+		
+		for(int i=0;i<a.length;i++){
+			
+			update(i+1,a[i]);
+		}
+		
+		for(int i :a)System.out.print(i+" ");
+		System.out.println("");
+		for(int i :bit)System.out.print(i+" ");
+		System.out.println("Sum of first 4 elements "+sum(4));
+		System.out.println("Sum of element from 3..6 "+(sum(6)-sum(2)));
+		
+	}
+	
 }
