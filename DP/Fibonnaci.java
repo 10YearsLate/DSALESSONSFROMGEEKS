9549a3a5581bde8f7bd3e328e1a9c877a367baa4
10YearsLate <souvik.itconsultant@gmail.com>
2019-11-21 16:18:03 +0000

    Fibo with DP

--- DP/Fibonnaci.java
+++ DP/Fibonnaci.java
@@ -1,18 +1,44 @@
-import java.lang.Systems.*;
+import java.lang.System.*;
+import java.util.*;
 
 public class Fibonnaci{
 
+    static int[] dp=new int[5000];
+    //Arrays.fill(dp,-1);
 
-  public static int fibonnaci(int N){
+    public static int fibodp(int N){   //0 ms
+
+        if(dp[N]!=0) return dp[N];
+        else{
+            if(N==1||N==2) dp[N]=1;
+            else{
+                dp[N]=fibodp(N-1)+fibodp(N-2);
+
+            }
+
+        }
+        return dp[N];
+
+    }
+
+
+  public static int fibonnaci(int N){     //398 ms
   
-        if(N==1 ||N==2) return 1;
-        else return (fibonnaci(N-1)+fibonacci(N-2));
+        if(N==1 || N==2) return 1;
+        else return (fibonnaci(N-1)+fibonnaci(N-2));
      
   }
   public static void main(String[] args){
-  
-      int N=5;
-      int output=fibonnaci(N);
+     
+      long start=System.nanoTime();
+      int N=4990;
+      //int output=fibonnaci(N);
+      int output=fibodp(N);
+      System.out.println(output);
+      long end=System.nanoTime();
+      //long time=(end-start); ///1000F;
+      System.out.println("Took: " + ((end - start) / 1000000) + "ms");
+
   
   }
 
